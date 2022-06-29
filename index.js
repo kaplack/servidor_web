89b8874c72c4c84420bf2d5ba89a31b0c7a559a4
Alan Burga <ahbb21@hotmail.com>
2022-06-29 18:34:10 -0500

    Desafio Servidor con Express

--- index.js
+++ index.js
@@ -1,16 +1,121 @@
-const http = require("http");
+// CLASE Coderhouse
+// const http = require("http");
 
-const server = http.createServer((peticion, respuesta) => {
-  const now = new Date();
-  let mensaje = "";
-  if (now.getHours() >= 6 && now.getHours() < 12) mensaje = "Buenos días";
-  else if (now.getHours() >= 12 && now.getHours() < 19)
-    mensaje = "Buenas tardes";
+// const server = http.createServer((peticion, respuesta) => {
+//   const now = new Date();
+//   let mensaje = "";
+//   if (now.getHours() >= 6 && now.getHours() < 12) mensaje = "Buenos días";
+//   else if (now.getHours() >= 12 && now.getHours() < 19)
+//     mensaje = "Buenas tardes";
 
-  //console.log("New request", new Date().toLocaleTimeString());
-  respuesta.end(mensaje);
-});
+//   //console.log("New request", new Date().toLocaleTimeString());
+//   respuesta.end(mensaje);
+// });
 
-const connectedServer = server.listen(8080, () => {
-  console.log(`Server running...`);
-});
+// const connectedServer = server.listen(8080, () => {
+//   console.log(`Server running...`);
+// });
+import fs from "fs";
+
+export class Contenedor {
+  constructor(archivo) {
+    (this.nombre = archivo), (this.id = 1);
+  }
+
+  save(objeto) {
+    try {
+      const files = fs.readdirSync("./data");
+      let data = fs.readFileSync(`./data/${this.nombre}`, "utf-8");
+
+      if (files.some((file) => file == this.nombre) && data != "") {
+        data = JSON.parse(data);
+        let ids = data.map((value) => {
+          return value.id;
+        });
+        const idn = parseInt(Math.max(...ids) + 1);
+        console.log(idn);
+        objeto = { ...objeto, id: idn };
+        data.push(objeto);
+        data = JSON.stringify(data);
+
+        fs.writeFileSync(`./data/${this.nombre}`, data, "utf-8");
+
+        console.log("si existe");
+      } else {
+        objeto = { ...objeto, id: this.id };
+        objeto = JSON.stringify(Array(objeto));
+        fs.writeFileSync(`./data/${this.nombre}`, objeto, "utf-8");
+        console.log("el archivo se creo");
+      }
+    } catch (error) {
+      console.log(error, "hubo un error");
+    }
+  }
+
+  getById(Number) {
+    try {
+      let data = fs.readFileSync(`./data/${this.nombre}`, "utf-8");
+      data = JSON.parse(data);
+      const idData = data.filter((item) => item.id == Number);
+      return idData;
+    } catch (error) {
+      console.log("hay un problema");
+    }
+  }
+
+  getAll() {
+    try {
+      let data = fs.readFileSync(`./data/${this.nombre}`, "utf-8");
+      data = JSON.parse(data);
+      return data;
+    } catch (error) {
+      console.log("hay un problema");
+    }
+  }
+
+  deleteById(Number) {
+    try {
+      let data = fs.readFileSync(`./data/${this.nombre}`, "utf-8");
+      data = JSON.parse(data);
+      let idData = data.filter((item) => item.id != Number);
+      idData = JSON.stringify(idData);
+      fs.writeFileSync(`./data/${this.nombre}`, idData, "utf-8");
+      return idData;
+    } catch (error) {
+      console.log("hay un problema");
+    }
+  }
+
+  deleteAll() {
+    fs.writeFileSync(`./data/${this.nombre}`, "", "utf-8");
+  }
+}
+
+// const test1 = new Contenedor("productos.txt");
+// item1 = {
+//   title: "escuadra",
+//   price: 123.45,
+//   thumbnail:
+//     "https://www.demaquinasyherramientas.com/wp-content/uploads/2012/12/Escuadra-Combinada.png",
+// };
+
+// item2 = {
+//   title: "lavadora",
+//   price: 350,
+//   thumbnail:
+//     "https://hiraoka.com.pe/media/mageplaza/blog/post/l/a/lavadora-consejos_5.jpg",
+// };
+// item3 = {
+//   title: "celular",
+//   price: 400,
+//   thumbnail: "https://f.rpp-noticias.io/2021/05/31/1101720img-2704jpg.jpg",
+// };
+
+// test1.deleteAll();
+// test1.save(item1);
+// test1.save(item2);
+// test1.save(item3);
+// console.log("el item con el id 2 es:", test1.getById(2));
+// //test1.deleteById(1);
+// console.log("Estos son todos los productos:", test1.getAll());
+// //test1.deleteAll();
--- server.js
+++ server.js
@@ -1,26 +1,56 @@
-const express = require("express");
+import express from "express";
+
+import { Contenedor } from "./index.js";
+
+const test1 = new Contenedor("productos.txt");
+const item1 = {
+  title: "escuadra",
+  price: 123.45,
+  thumbnail:
+    "https://www.demaquinasyherramientas.com/wp-content/uploads/2012/12/Escuadra-Combinada.png",
+};
+
+const item2 = {
+  title: "lavadora",
+  price: 350,
+  thumbnail:
+    "https://hiraoka.com.pe/media/mageplaza/blog/post/l/a/lavadora-consejos_5.jpg",
+};
+const item3 = {
+  title: "celular",
+  price: 400,
+  thumbnail: "https://f.rpp-noticias.io/2021/05/31/1101720img-2704jpg.jpg",
+};
+
+//test1.deleteAll();
+// test1.save(item1);
+// test1.save(item2);
+// test1.save(item3);
 
 const app = express();
 const PORT = process.env.PORT || 8080;
 let visitas = 0;
 
 app.get("/", (request, response) => {
-  response.send("<h1>Hola Insecto!</h1>");
+  response.send(test1.getAll());
 });
 
-app.get("/tutores", (request, response) => {
-  response.send({ mensaje: "Saludos Tutores!" });
+app.get("/productoRandom", (request, response) => {
+  let id = Math.floor(Math.random() * 3) + 1;
+  response.send(test1.getById(id));
+  //console.log(`Id: ${id}`);
 });
 
-app.get("/hora", (req, res) => {
-  res.send(new Date().toLocaleTimeString());
-});
-app.get("/visita", (req, res) => {
-  visitas++;
-  res.send(`${visitas}`);
-});
+// app.get("/hora", (req, res) => {
+//   res.send(new Date().toLocaleTimeString());
+// });
+// app.get("/visita", (req, res) => {
+//   visitas++;
+//   res.send(`Visitantes: ${visitas}`);
+// });
 
 const server = app.listen(PORT, () => {
   console.log(`Server listening [${PORT}]...`);
 });
+
 server.on("error", (e) => console.log(`Error on server.`, e));
